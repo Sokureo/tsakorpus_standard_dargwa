a6c6abb5d81c8c5b1972396ea7b423a79be0c551
Sokureo <elena.o.sokur@gmail.com>
2023-12-30 20:55:23 +0300

    start page'

--- search/web_app/views.py
+++ search/web_app/views.py
@@ -22,7 +22,10 @@ from .search_pipelines import *
 
 @app.route('/')
 def start_page():
-    return render_template('start.html')
+    return render_template('start_page.html',
+                           locale=get_locale(),
+                           locales=settings.interface_languages,
+                           )
 
 
 @app.route('/search')
